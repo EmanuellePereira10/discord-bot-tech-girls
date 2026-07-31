d4ad9126d859db1ebce7eb59dfb7e3307bef66b4
Thais Attico <contato.thaisattico@gmail.com>
2026-07-31 12:33:15 -0300

    Adicionado variável id_noticia

--- src/database/database.py
+++ src/database/database.py
@@ -1,14 +1,13 @@
 import sqlite3
 
-def inserir_noticia(titulo, autor, url, postado_em, tag):
+def inserir_noticia(id_noticia, titulo, autor, url, postado_em, tag):
     conexao = sqlite3.connect("bot.db")
     cursor = conexao.cursor()
 
     try:
-        cursor.execute("INSERT INTO noticias_postadas (titulo, autor, url, postado_em, tag) VALUES (?, ?, ?, ?, ?)", (titulo, autor, url, postado_em, tag))
+        cursor.execute("INSERT INTO noticias_postadas (id_noticia, titulo, autor, url, postado_em, tag) VALUES (?, ?, ?, ?, ?, ?)", (id_noticia, titulo, autor, url, postado_em, tag))
         conexao.commit()
-        id_gerado = cursor.lastrowid
-        return ("noticia aceita", id_gerado)
+        return ("noticia aceita", id_noticia)
     except:
         cursor.execute("SELECT id_noticia FROM noticias_postadas WHERE url = ?", (url,))
         resultado = cursor.fetchone()
