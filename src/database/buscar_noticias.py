6d76286b53d90aefd3b5fb658ad8cd10588c3aa0
Thais Attico <contato.thaisattico@gmail.com>
2026-08-01 18:06:52 -0300

    Rename function buscar_noticia to buscar_noticias

--- src/database/buscar_noticias.py
+++ src/database/buscar_noticias.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-def buscar_noticia(id_noticia):
+def buscar_noticias(id_noticia):
     conexao = sqlite3.connect("bot.db")
     cursor = conexao.cursor()
     cursor.execute("SELECT id_noticia, titulo, autor, url, postado_em, tag FROM noticias_postadas WHERE id_noticia = ?", (id_noticia,))
